e0dd8a1cee353e6f438d62ff616f6cc323f575d8
SDMckone <seandmckone@gmail.com>
2023-09-28 21:04:13 -0400

    Clean up PersonalDetails

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -1,49 +1,48 @@
 function PersonalDetails({ personalDetails, onChange }) {
   return (
-    <div id="personal-details">
+    <form action="/submit" method="post" id="personal-details-form">
       <h2>Personal Details</h2>
-      <form action="/submit" method="post" id="personal-details-form">
-        <label htmlFor="full-name">Full name</label>
-        <input
-          type="text"
-          placeholder="First and last name"
-          id="full-name"
-          onChange={onChange}
-          data-key="fullName"
-          value={personalDetails.fullName}
-        />
 
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          placeholder="Enter email"
-          id="email"
-          onChange={onChange}
-          data-key="email"
-          value={personalDetails.email}
-        />
+      <label htmlFor="full-name">Full name</label>
+      <input
+        type="text"
+        placeholder="First and last name"
+        id="full-name"
+        onChange={onChange}
+        data-key="fullName"
+        value={personalDetails.fullName}
+      />
 
-        <label htmlFor="phone-number">Phone number</label>
-        <input
-          type="tel"
-          placeholder="Enter phone number"
-          id="phone-number"
-          onChange={onChange}
-          data-key="phoneNumber"
-          value={personalDetails.phoneNumber}
-        />
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        placeholder="Enter email"
+        id="email"
+        onChange={onChange}
+        data-key="email"
+        value={personalDetails.email}
+      />
 
-        <label htmlFor="address">Address</label>
-        <input
-          type="text"
-          placeholder="City, Country"
-          id="address"
-          onChange={onChange}
-          data-key="address"
-          value={personalDetails.address}
-        />
-      </form>
-    </div>
+      <label htmlFor="phone-number">Phone number</label>
+      <input
+        type="tel"
+        placeholder="Enter phone number"
+        id="phone-number"
+        onChange={onChange}
+        data-key="phoneNumber"
+        value={personalDetails.phoneNumber}
+      />
+
+      <label htmlFor="address">Address</label>
+      <input
+        type="text"
+        placeholder="City, Country"
+        id="address"
+        onChange={onChange}
+        data-key="address"
+        value={personalDetails.address}
+      />
+    </form>
   );
 }
 
