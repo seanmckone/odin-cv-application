947ab314106cded59c5037fb07e34fe96173624b
SDMckone <seandmckone@gmail.com>
2023-10-01 17:43:53 -0400

    Update App with temp data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./styles/App.css";
-import PersonalDetails from "./components/PersonalDetails.jsx";
 import { useState } from "react";
-import EducationForm from "./components/EducationForm";
+import PersonalDetails from "./components/PersonalDetails.jsx";
+import EducationForm from "./components/EducationForm.jsx";
+import ExperienceForm from "./components/ExperienceForm.jsx";
 
 function App() {
   const [personalDetails, setPersonalDetails] = useState({
@@ -19,6 +20,15 @@ function App() {
     location: "Raleigh, NC",
   });
 
+  const [experienceDetails, setExperienceDetails] = useState({
+    company: "Cool Company",
+    position: "SWE I",
+    startDate: "May 2021",
+    endDate: "May 2023",
+    location: "Raleigh, NC",
+    description: "Did some work on giant codebase. Very fun.",
+  });
+
   function handlePersonalDetailsChange(e) {
     setPersonalDetails({
       ...personalDetails,
@@ -33,6 +43,13 @@ function App() {
     });
   }
 
+  function handleExperienceDetailsChange(e) {
+    setExperienceDetails({
+      ...experienceDetails,
+      [e.target.dataset.key]: e.target.value,
+    });
+  }
+
   return (
     <>
       <PersonalDetails
@@ -43,6 +60,10 @@ function App() {
         educationDetails={educationDetails}
         onChange={handleEducationDetailsChange}
       ></EducationForm>
+      <ExperienceForm
+        experienceDetails={experienceDetails}
+        onChange={handleExperienceDetailsChange}
+      ></ExperienceForm>
     </>
   );
 }
