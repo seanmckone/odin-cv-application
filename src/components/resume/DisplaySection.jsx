7b27d49c184dc766a1160567814ff93b15f689d0
SDMckone <seandmckone@gmail.com>
2023-10-04 18:53:02 -0400

    Update DisplaySection to hide hidden sections

--- src/components/resume/DisplaySection.jsx
+++ src/components/resume/DisplaySection.jsx
@@ -1,9 +1,11 @@
 import Section from "./Section";
 
 function DisplaySection({ section }) {
-  return section.map((sectionData) => (
-    <Section key={sectionData.id} sectionData={sectionData}></Section>
-  ));
+  return section.map((sectionData) => {
+    if (!sectionData.isHidden) {
+      return <Section key={sectionData.id} sectionData={sectionData}></Section>;
+    }
+  });
 }
 
 export default DisplaySection;
